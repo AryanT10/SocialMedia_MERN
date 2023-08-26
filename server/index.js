ff3d374715c200295d69eb697c1324828064a1a6
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-08-26 19:21:30 -0400

    feat; update mongoose Schema

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from "path";
 import { fileURLToPath } from 'url';
-
+import {register} from "./controllers/auth.js"
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +33,10 @@ const storage = multer.diskStorage({
 		cb(null, file.originalname);
 	}
 });
+
+// ROUTES WITH FILES
+ //  upload.single("picture") : middleware, register: actual logic
+app.post("/auth/register", upload.single("picture"), register);
 
 const upload = multer({ storage });
 

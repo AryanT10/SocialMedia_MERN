41b76a3c877674b150c5fb6d099c862d60c2c332
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-08-30 14:21:05 -0400

    feat: edited navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -9,7 +9,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { setMode, setLogout } from "state";
 import { useNavigate } from "react-router-dom";
 import FlexBetween from '../../components/flexBetween';
-import { red } from '@mui/material/colors';
 
 const Navbar = () => {
 	const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);

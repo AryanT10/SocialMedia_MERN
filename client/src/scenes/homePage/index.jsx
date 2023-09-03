cd3262b6ad1f00fe2803841f61159c6c691d68af
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-09-03 01:11:52 -0400

    feat: add setPost , post , getPost .. uploadImage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from '@mui/material';
 import Navbar from '../navbar';
 import { useSelector } from 'react-redux';
 import UserWidget from '../widgets/UserWidget';
+import MyPostWidget from '../widgets/MyPostWidget';
 
 const HomePage = () => {
 	const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -24,13 +25,13 @@ const HomePage = () => {
 					flexBasis={isNonMobileScreens ? "42%" : undefined}
 					mt={isNonMobileScreens ? undefined : "2rem"}
 				>
-
+					<MyPostWidget picturePath={picturePath} />
 				</Box>
 				{isNonMobileScreens && (
 					<Box flexBasis="26%">
-						
+
 					</Box>
-					
+
 				)}
 			</Box>
 		</Box>

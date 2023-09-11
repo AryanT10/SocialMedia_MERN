9b3299805766e31c9f5d515a302cba054bf43773
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-09-10 21:56:23 -0400

    feat: check

--- server/index.js
+++ server/index.js
@@ -25,17 +25,23 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin: "https://sociogram-aryant10.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: "https://sociogram-aryant10.vercel.app",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+// }));
 
 
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://sociogram-aryant10.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  }
+));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */

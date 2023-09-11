35062b8f2f65eecb1700854da45d099e6dc6050b
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-09-10 22:48:02 -0400

    fix : error

--- server/index.js
+++ server/index.js
@@ -23,7 +23,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: false,
+}));
 app.use(express.json());
 
 // app.use(cors({

a795eafd6dc3b7e0beb0e3d616c2259c5886ec7a
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-09-10 22:59:14 -0400

    feat: fix

--- server/index.js
+++ server/index.js
@@ -25,14 +25,15 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: false,
+  origin: true,
 }));
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+app.options('/login', function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.end();
+});
 
 // app.use(cors({
 //   origin: "https://sociogram-aryant10.vercel.app",
